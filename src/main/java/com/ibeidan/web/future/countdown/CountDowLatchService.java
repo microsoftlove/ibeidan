8eeb7f2ba84ca6682c5f0dafabbd0df83703a5fc
李贝贝 <beibei.li@ishansong.com>
2020-01-06 16:53:07 +0800

    countdwolatch 测试

--- src/main/java/com/ibeidan/web/future/countdown/CountDowLatchService.java
+++ src/main/java/com/ibeidan/web/future/countdown/CountDowLatchService.java
@@ -8,6 +8,8 @@ import java.util.concurrent.CountDownLatch;
  *
  * Latch译为门闩的意思，当门闩没有打开时，N个人是不能进入屋内的。也就是不能继续向下运行的，支持这样的
  * 特性，可以控制线程执行任务的时机，使线程以"组团"的方式一起执行任务。
+ * await(5, TimeUnit.SECONDS) 使线程在指定的最大时间单位内进入waiting状态，
+ * 如果超过这个时间则自动唤醒，程序继续向下执行。
  */
 public class CountDowLatchService {
 
@@ -29,12 +31,12 @@ public class CountDowLatchService {
     }
 
 
-    public void downMethod(){
+     void downMethod(){
         System.out.println("X");
         countDownLatch.countDown();
     }
 
-    public void testDown(){
+     void testDown(){
         System.out.println(countDownLatch.getCount()+"=="+Thread.currentThread().getName()+"开始执行");
         try {
             Thread.sleep(2000);
